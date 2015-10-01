b509a9c8bc76e6d316ec4cb4b4f930901efc4cb0
Jeffrey DeYoung <jeffrey.deyoung@pearson.com>
2015-10-01 09:27:36 -0600

    encryption and decryption working

--- backendCode/GhostPasswordBackend/src/main/java/com/github/ghostpassword/ghostpasswordbackend/EncryptUtils.java
+++ backendCode/GhostPasswordBackend/src/main/java/com/github/ghostpassword/ghostpasswordbackend/EncryptUtils.java
@@ -1,13 +1,26 @@
 package com.github.ghostpassword.ghostpasswordbackend;
 
+import org.springframework.security.crypto.encrypt.Encryptors;
+import org.springframework.security.crypto.encrypt.TextEncryptor;
+
 /**
  *
  * @author udeyoje
  */
 public class EncryptUtils
 {
-    public static String encyptStringWithAES(String password, String toEncrypt){
+
+    public static String encryptStringWithAES(String salt, String password, String toEncrypt)
+    {        
+        TextEncryptor encryptor = Encryptors.text(password, salt);
+        String encryptedText = encryptor.encrypt(toEncrypt);
+        return encryptedText;
+    }
     
-        throw new UnsupportedOperationException("Not done yet.");
+    public static String decryptStringWithAES(String salt, String password, String toDecrypt)
+    {        
+        TextEncryptor decryptor = Encryptors.text(password, salt);
+        String decryptedText = decryptor.decrypt(toDecrypt);
+        return decryptedText;
     }
 }
