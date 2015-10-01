1d290d09d51fb30a23fa4820bd04197689592342
Jeffrey DeYoung <jeffrey.deyoung@pearson.com>
2015-10-01 08:33:50 -0600

    first code

---
+++ backendCode/GhostPasswordBackend/src/com/github/ghostpassword/backend/EncryptUtils.java
@@ -0,0 +1,14 @@
+
+package com.github.ghostpassword.backend;
+
+/**
+ *
+ * @author udeyoje
+ */
+public class EncryptUtils
+{
+    public static String encyptStringWithAES(String password, String toEncrypt){
+    
+        throw new UnsupportedOperationException("Not done yet.");
+    }
+}
