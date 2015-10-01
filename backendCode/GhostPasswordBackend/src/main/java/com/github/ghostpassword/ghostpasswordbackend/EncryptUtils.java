5cea6cc1c8ca1a5a660c5c4dcb06660cc737a552
Jeffrey DeYoung <jeffrey.deyoung@pearson.com>
2015-10-01 08:56:49 -0600

    refactor to maven project

---
+++ backendCode/GhostPasswordBackend/src/main/java/com/github/ghostpassword/ghostpasswordbackend/EncryptUtils.java
@@ -0,0 +1,13 @@
+package com.github.ghostpassword.ghostpasswordbackend;
+
+/**
+ *
+ * @author udeyoje
+ */
+public class EncryptUtils
+{
+    public static String encyptStringWithAES(String password, String toEncrypt){
+    
+        throw new UnsupportedOperationException("Not done yet.");
+    }
+}
